cdc5da3e360b3f63e7e457d1b9620292c0661c87
Nguyễn Lưu Bảo Khang <143237132+khangdevv@users.noreply.github.com>
2025-10-27 09:11:18 +0700

    Fibonacci (#3)
    * Fibonacci
    
    * Them main

--- Source.cpp
+++ Source.cpp
@@ -19,6 +19,14 @@ bool kiemTraSoHoanChinh(int n) {
     return tong == n;
 }
 float thuong(int a, int b);
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 bool KTSNT(int n) {
     if (n < 2) return false;
     if (n % 2 == 0) return n == 2;
@@ -27,14 +35,16 @@ bool KTSNT(int n) {
     }
     return true;
 }
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Sinh Vien A thuc hien tinh tong : " << tong(2,3);
 	cout << "Sinh Vien B thuc hien tinh hieu : " << hieu(2,3);
 	cout << "Sinh Vien C thuc hien tinh tich : " << tich(2,3);
+	cout << "Sinh Vien A thuc hien tinh fibonacci : " << fibonacci(4);
 	cout << "Sinh Vien B thuc hien kt 5 la so nguyen to: " << KTSNT(5);
-	cout << "Sinh Vien C thuc hien KT so hoan thien: "<<kiemTraSoHoanChinh(6);
+	cout << "Sinh Vien C thuc hien KT so hoan thien: "<< kiemTraSoHoanChinh(6);
 	system("pause");
 	return 0;
 }
