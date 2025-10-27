ba618d5c8e6307ef0e5977403814f3b9059d104f
Đinh Viết Huy <143201537+nikanika22@users.noreply.github.com>
2025-10-27 09:09:38 +0700

    Sinh vien B thuc hien kt SNT (#2)
    merge nhe anh khang

--- Source.cpp
+++ Source.cpp
@@ -19,12 +19,21 @@ bool kiemTraSoHoanChinh(int n) {
     return tong == n;
 }
 float thuong(int a, int b);
+bool KTSNT(int n) {
+    if (n < 2) return false;
+    if (n % 2 == 0) return n == 2;
+    for (int i = 3; i <= n / i; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Sinh Vien A thuc hien tinh tong : " << tong(2,3);
 	cout << "Sinh Vien B thuc hien tinh hieu : " << hieu(2,3);
 	cout << "Sinh Vien C thuc hien tinh tich : " << tich(2,3);
+	cout << "Sinh Vien B thuc hien kt 5 la so nguyen to: " << KTSNT(5);
 	cout << "Sinh Vien C thuc hien KT so hoan thien: "<<kiemTraSoHoanChinh(6);
 	system("pause");
 	return 0;
