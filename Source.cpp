b642972fa3ab14c233d0a2b60280b7a4acd5cfb1
nikanika <dinhhuyteam@gmail.com>
2025-10-27 09:02:54 +0700

    Sinh vien B thuc hien kt SNT

--- Source.cpp
+++ Source.cpp
@@ -10,12 +10,21 @@ int tich(int a, int b){
 	return a * b;
 }
 float thuong(int a, int b);
+bool KTSNT(int n) {
+    if (n < 2) return false;
+    if (n % 2 == 0) return n == 2;
+    for (int i = 3; i <= n / i; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Sinh Vien A thuc hien tinh tong : " << tong(2,3);
 	cout << "Sinh Vien B thuc hien tinh hieu : " << hieu(2,3);
 	cout << "Sinh Vien C thuc hien tinh tich : " << tich(2,3);
+	cout << "Sinh Vien B thuc hien kt 5 la so nguyen to: " << KTSNT(2,3);
 	system("pause");
 	return 0;
 }
