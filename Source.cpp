15b40c020fe559c63ddc76e535fe50aa45cbf290
nikanika <dinhhuyteam@gmail.com>
2025-10-27 08:35:50 +0700

    Sinh vien B thuc hien tinh hieu

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-
 using namespace std;
 int tong(int a, int b) {
     return a + b;
 }
-int hieu(int a, int b);
+int hieu(int a, int b) {
+    return a - b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Sinh Vien A thuc hien tinh tong : " << tong(2,3);
+	cout << "Sinh Vien B thuc hien tinh hieu : " << hieu(2,3);
 	system("pause");
 	return 0;
 }
