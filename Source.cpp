05a97237fceeeeaf9e612a7df60025402a3033dd
hieupeko <hieu654789@gmail.com>
2025-10-27 08:38:22 +0700

    Sinh vien C thuc hien tinh tich

--- Source.cpp
+++ Source.cpp
@@ -6,13 +6,16 @@ int tong(int a, int b) {
 int hieu(int a, int b) {
     return a - b;
 }
-int tich(int a, int b);
+int tich(int a, int b){
+	return a * b;
+}
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Sinh Vien A thuc hien tinh tong : " << tong(2,3);
 	cout << "Sinh Vien B thuc hien tinh hieu : " << hieu(2,3);
+	cout << "Sinh Vien C thuc hien tinh tich : " << tich(2,3);
 	system("pause");
 	return 0;
 }
